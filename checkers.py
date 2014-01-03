adea2975442d72f384cde02a1f8bb249a7d19312
OddLlama <oddllamaprod@gmail.com>
2014-01-02 18:12:01 -0600

    fixed bugs, debug output

--- checkers.py
+++ checkers.py
@@ -17,19 +17,25 @@ class Board:
     def __init__(self):
         """Constructs a new, normal set up board."""
         self.data = []
-        self.data.append(start_rows(Checker.PLAYER_ONE))
-        self.data.append(empty_rows(2))
-        self.data.append(start_rows(Checker.PLAYER_TWO))
+        self.data.append(Board.start_rows(Checker.PLAYER_ONE))
+        self.data.append(Board.empty_rows(2))
+        self.data.append(Board.start_rows(Checker.PLAYER_TWO))
 
+    @staticmethod
     def start_rows(player):
         """The configuration of pieces that the game starts with."""
         r1, r2, r3 = [], [], []
         for _ in range(4):
-            r1.append([None, Cheker(player)])
-            r2.append([Cheker(player), None])
-            r3.append([None, Cheker(player)])
+            r1.append([None, Checker(player)])
+            r2.append([Checker(player), None])
+            r3.append([None, Checker(player)])
         return [r1, r2, r3]
 
+    @staticmethod
     def empty_rows(count):
         """Returns an amount of empty rows."""
         return [[None] * 8 for _ in range(count)]
+
+if __name__ == '__main__':
+    board = Board()
+    print(board.data)
